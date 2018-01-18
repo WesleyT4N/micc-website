2537906a8aad83361c5c801cb9b0b7f2808ddcfd
Wes T4N <wesleyt1998@gmail.com>
2018-01-18 15:17:52 -0500

    file setup

---
+++ includes/header.php
@@ -0,0 +1,23 @@
+<?php session_start(); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Mutual Investment Club of Cornell</title>
+  <meta name="description" content="Cornell's largest undergraduate investment fund with over $70,000 under management."/>
+  <link rel="stylesheet" href="./css/style.min.css">
+
+  <script
+  src="https://code.jquery.com/jquery-3.2.1.min.js"
+  integrity="sha256-hwg4gsxgFZhOsEEamdOYGBf13FyQuiTwlAQgxVSNgt4="
+  crossorigin="anonymous"></script>
+  <script src="./js/main.js"></script>
+</head>
+<header>
+  <nav>
+    
+  </nav>
+</header>
+<body>
